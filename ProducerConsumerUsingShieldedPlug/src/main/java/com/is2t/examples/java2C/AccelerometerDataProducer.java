29c9dc1e927902503697fc9f65e535fea553536a
Xavier Sautejeau <xsautejeau@is2t-intern.com>
2015-10-07 10:08:59 +0200

    added missing copyright notices

--- ProducerConsumerUsingShieldedPlug/src/main/java/com/is2t/examples/java2C/AccelerometerDataProducer.java
+++ ProducerConsumerUsingShieldedPlug/src/main/java/com/is2t/examples/java2C/AccelerometerDataProducer.java
@@ -1,3 +1,11 @@
+/**
+ *
+ * Java
+ *
+ * Copyright 2015 IS2T. All rights reserved.
+ * IS2T PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ *
+ */
 package com.is2t.examples.java2C;
 
 import com.is2t.sp.ShieldedPlug;
