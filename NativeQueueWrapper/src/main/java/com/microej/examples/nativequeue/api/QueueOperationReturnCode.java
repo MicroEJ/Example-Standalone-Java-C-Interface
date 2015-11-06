bfd49d4999d6c12f937f7502f237f21e5b93ac45
Xavier Sautejeau <xsautejeau@is2t-intern.com>
2015-11-06 09:49:26 +0100

    synchronized QueueOperationReturnCode constants with LLNativeQueueService.h::queue_operation_return_code_t

--- NativeQueueWrapper/src/main/java/com/microej/examples/nativequeue/api/QueueOperationReturnCode.java
+++ NativeQueueWrapper/src/main/java/com/microej/examples/nativequeue/api/QueueOperationReturnCode.java
@@ -6,6 +6,6 @@ public interface QueueOperationReturnCode {
 	final static int QUEUE_REGISTERED = 2;
 	final static int QUEUE_READ_OK = 3;
 	final static int QUEUE_READ_FAILED = 4;
-	final static int QUEUE_WRITE_OK = 4;
-	final static int QUEUE_WRITE_FAILED = 4;
+	final static int QUEUE_WRITE_OK = 5;
+	final static int QUEUE_WRITE_FAILED = 6;
 }
