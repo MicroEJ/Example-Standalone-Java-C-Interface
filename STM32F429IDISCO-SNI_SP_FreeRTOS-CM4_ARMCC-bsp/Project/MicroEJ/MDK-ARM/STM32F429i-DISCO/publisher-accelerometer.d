b1f7498a838a52254366733610d6a66766bd2d79
Xavier Sautejeau <xsautejeau@is2t-intern.com>
2015-11-19 09:51:51 +0100

    - removed MicroVision build files from version control

--- STM32F429IDISCO-SNI_SP_FreeRTOS-CM4_ARMCC-bsp/Project/MicroEJ/MDK-ARM/STM32F429i-DISCO/publisher-accelerometer.d
+++
@@ -1,8 +0,0 @@
-.\stm32f429i-disco\publisher-accelerometer.o: ..\..\..\..\ProducerConsumerUsingShieldedPlug\src\main\c\publisher-accelerometer.c
-.\stm32f429i-disco\publisher-accelerometer.o: ..\..\..\..\ProducerConsumerUsingShieldedPlug\src\main\c\publisher-accelerometer.h
-.\stm32f429i-disco\publisher-accelerometer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\stm32f429i-disco\publisher-accelerometer.o: ..\..\..\..\ProducerConsumerUsingShieldedPlug\src\main\c\publisher.h
-.\stm32f429i-disco\publisher-accelerometer.o: ..\includeAPIs\sp.h
-.\stm32f429i-disco\publisher-accelerometer.o: ..\..\..\..\ProducerConsumerUsingShieldedPlug\src\main\c\accelerometer-data.h
-.\stm32f429i-disco\publisher-accelerometer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
-.\stm32f429i-disco\publisher-accelerometer.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdlib.h
