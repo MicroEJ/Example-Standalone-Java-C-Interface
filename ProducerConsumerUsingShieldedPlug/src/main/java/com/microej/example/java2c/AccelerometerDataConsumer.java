25e779d90227fbfde661301d4aa932bd3fed069d
Xavier Sautejeau <xsautejeau@is2t-intern.com>
2017-01-13 18:05:20 +0100

    WI18748 rely on the jpf downloadable from developer.microej.com instead of providing a custom one

---
+++ ProducerConsumerUsingShieldedPlug/src/main/java/com/microej/example/java2c/AccelerometerDataConsumer.java
@@ -0,0 +1,49 @@
+/*
+ *
+ * Java
+ *
+ * Copyright 2016 IS2T. All rights reserved.
+ * For demonstration purpose only.
+ * IS2T PROPRIETARY. Use is subject to license terms.
+ *
+ */
+package com.microej.example.java2c;
+
+import ej.sp.EmptyBlockException;
+import ej.sp.ShieldedPlug;
+
+public class AccelerometerDataConsumer implements Runnable{
+
+	private final int accelerometerID;
+
+	public AccelerometerDataConsumer(int accelerometerID) {
+		super();
+		this.accelerometerID = accelerometerID;
+	}
+
+
+	@Override
+	public void run() {
+
+		ShieldedPlug database = ShieldedPlug.getDatabase(ProducerConsumerExample.DATABASE_ID);
+		database.setReader(accelerometerID, new AccelerometerDataUnmarshaller());
+
+		while (true) {
+			try {
+				// wait for a change on accelerometer data
+				database.waitFor(accelerometerID);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			try {
+				// read the AccelerometerData
+				AccelerometerData accelerometerData = (AccelerometerData) database.readObject(accelerometerID);
+				System.out.println("-" + accelerometerData.toString());
+			} catch (EmptyBlockException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+
+}
